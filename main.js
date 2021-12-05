4bdf1e2d596129b37b8e9ee5a960c37f372ae793
ykgautam09 <yateeshgautam1357@gmail.com>
2021-12-05 11:32:31 +0530

    replace files with current one

--- main.js
+++ main.js
@@ -1,46 +1,60 @@
-function CreateObject(path) {
-    this.play = new Audio(path).play()
-
+for (var i = 0; i < document.querySelectorAll('.drum').length; i++) {
+    document.querySelectorAll('.drum')[i].addEventListener('click', function () {
+        var buttonInnerHTML=this.innerHTML;
+        sound(buttonInnerHTML);
+        animation(buttonInnerHTML);
+    });
 }
-path = ''
-function applyListener() {
-    function onClick() {
-        for (var i = 0; i < 8; i++) {
-            document.querySelectorAll('img')[i].addEventListener('click', function () {
-                path = './sounds/' + this.alt + '.wav'
-                CreateObject(path).play()
-            })
-        }
+document.addEventListener('keypress',function (){
+    sound(event.key);
+    animation(event.key);
+})
+
+function sound(input){
+    switch(input)
+    {
+        case 'w':
+            var tom1=new Audio('sounds/tom-1.mp3')
+            tom1.play();
+            break;
+        case 'a':
+                var tom2=new Audio('sounds/tom-2.mp3')
+                tom2.play();
+                break;
+
+
+        case 's':
+                var tom3=new Audio('sounds/tom-3.mp3')
+                tom3.play();
+                break;
+
+        case 'd':
+                var tom4=new Audio('sounds/tom-4.mp3')
+                tom4.play();
+                break;
+
+        case 'j':
+                var snare=new Audio('sounds/snare.mp3')
+                snare.play();
+                break;
+
+        case 'k':
+                var crash=new Audio('sounds/crash.mp3')
+                crash.play();
+                break;
+
+        case 'l':
+                var kick=new Audio('sounds/kick-bass.mp3')
+                kick.play();
+                break;
+        
+                default:
+                    console.log(buttonInnerHTML)
     }
-    let keyPath = ''
-    function onKeyPress() {
-        document.addEventListener('keydown', function (event) {
-            if (event.key === 'l')
-                keyPath = 'crash'
-            else if (event.key === 's')
-                keyPath = 'highHat'
-            else if (event.key === 'a')
-                keyPath = 'ride'
-            else if (event.key === 'd')
-                keyPath = 'snare'
-            else if (event.key === 'n')
-                keyPath = 'lowTom'
-            else if (event.key === 'c')
-                keyPath = 'bass'
-            else if (event.key === 'j')
-                keyPath = 'highTom'
-            else if (event.key === 'k')
-                keyPath = 'floorTom'
-            else
-                keyPath = ''
-
-
-            path = './sounds/' + keyPath + '.wav'
-            new CreateObject(path).play()
-        })
-    }
-    onClick()
-    onKeyPress()
-
 }
-applyListener()
+
+function animation(item){
+document.querySelector('.'+item).classList.add('pressed');
+setTimeout(function (){document.querySelector('.'+item).classList.remove('pressed');
+},300);
+}
